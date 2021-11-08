510cbd9b92fde67a4f64b4dc3f5b34260ed132a0
kskish <kskish18@gmail.com>
2021-11-08 14:51:59 -0500

    Project setup

---
+++ react-notes-app/src/components/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <span>Hello this is our first Note</span>
+    </div>
+  );
+};
+
+export default Note;
