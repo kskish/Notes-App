b57b4fb0d34bc89e05d7e2c9ccd08eedec59ba0a
kskish <kskish18@gmail.com>
2021-11-08 20:45:12 -0500

    Search Bar

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -31,6 +32,8 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState("");
+
   const deleteNote = (id) => {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
@@ -38,8 +41,11 @@ const App = () => {
 
   return (
     <div className="container">
+      <Search handleSearchNote={setSearchText} />
       <NotesList
-        notes={notes}
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
         setNotes={setNotes}
         handleDeleteNote={deleteNote}
       />
